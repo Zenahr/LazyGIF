b04d234890d17251e0a80d43256a4f2b7fc360f5
Zenahr <zenmatica@gmail.com>
2021-11-19 22:54:59 +0100

    add advanced controls

--- player.py
+++ player.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
-# C:\Users\STUDIUM\Videos\Apex Legends\CHEATER FOOTAGE
+# GET THE SLIDER TO WORK ON MOUSE CLICK ANYWHERE: https://stackoverflow.com/questions/52689047/moving-qslider-to-mouse-click-position
+
+
+"""ROADMAP
+
+hotkeys for seeking
+----------------------------------------------------------------------------------------------------
+arrow   keys        : seek 10   frames  forward or backward
+arrow   keys + shift: seek 100  frames  forward or backward
+arrow   keys + ctrl : seek 1    frame   forward or backward
+
+
+
+"""
+
 
 """
 InnoSetup: e process of adding custom verbs to a file's shortcut menu is described in the Extending Shortcut Menus and Verbs and File Associations topics. If you prefer a simpler way, you can also just put a shortcut to your EXE in the SendTo folder.
@@ -9,8 +23,20 @@ InnoSetup: e process of adding custom verbs to a file's shortcut menu is describ
 from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
-        QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QSizePolicy,
+    QSlider,
+    QStyle,
+    QVBoxLayout,
+    QWidget,
+    QLineEdit,
+    QCheckBox
+        )
 from PyQt5.QtWidgets import QMainWindow,QWidget, QPushButton, QAction
 from PyQt5.QtGui import QIcon
 import sys
@@ -18,18 +44,24 @@ import os
 
 class VideoWindow(QMainWindow):
 
+    def enableAllControls(self, flag=True):
+        self.startMarkerTime.setEnabled(flag)
+        self.endMarkerTime.setEnabled(flag)
+        self.symmetrizeCheckbox.setEnabled(flag)
+        self.playButton.setEnabled(flag)
+        self.convertToGifButton.setEnabled(flag)
+
     def __init__(self, parent=None):
         super(VideoWindow, self).__init__(parent)
         self.setWindowTitle("InstaGif - Instantly Create GIFs") 
 
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface) # https://doc.qt.io/qtforpython-5/PySide2/QtMultimedia/QMediaPlayer.html#qmediaplayer
         self.mediaPlayer.setNotifyInterval(30) # XYms refresh rate (needed for notifs)
         self.mediaPlayer.setMuted(True) # mute the video
 
         videoWidget = QVideoWidget()
 
         self.playButton = QPushButton()
-        self.playButton.setEnabled(False)
         self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
         self.playButton.clicked.connect(self.play)
 
@@ -41,21 +73,37 @@ class VideoWindow(QMainWindow):
         self.errorLabel.setSizePolicy(QSizePolicy.Preferred,
                 QSizePolicy.Maximum)
 
+        # Other stuff
+        self.startMarkerTime    = QLineEdit(self)
+        self.startMarkerTime.setEnabled(False)
+        self.startMarkerTime.setPlaceholderText("mm:ss:ms")
+
+        self.endMarkerTime      = QLineEdit(self)
+        self.endMarkerTime.setEnabled(False)
+        self.endMarkerTime.setPlaceholderText("mm:ss:ms")
+
+        self.symmetrizeCheckbox = QCheckBox("Symmetrize")
+        self.symmetrizeCheckbox.setEnabled(False)
+        self.symmetrizeCheckbox.setChecked(False)
+
+        self.convertToGifButton = QPushButton("Save GIF")
+        self.convertToGifButton.clicked.connect(self.saveAsGif)
+
         # Create new action
-        openAction = QAction(QIcon('open.png'), '&Open', self)        
+        openAction = QAction(QIcon('open.png'), '&Open Video', self)        
         openAction.setShortcut('Ctrl+O')
-        openAction.setStatusTip('Import Video')
+        openAction.setStatusTip('')
         openAction.triggered.connect(self.openFile)
 
         # Create exit action
         exitAction = QAction(QIcon('exit.png'), '&Exit', self)        
         exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
+        exitAction.setStatusTip('')
         exitAction.triggered.connect(self.exitCall)
 
         # dev tool button
         devFileAction = QAction(QIcon('open.png'), '&Devtest', self)        
-        devFileAction.setShortcut('Ctrl+R')
+        devFileAction.setShortcut('R')
         devFileAction.setStatusTip('')
         devFileAction.triggered.connect(self.openTestFile)
 
@@ -80,6 +128,11 @@ class VideoWindow(QMainWindow):
         layout.addWidget(videoWidget)
         layout.addLayout(controlLayout)
         layout.addWidget(self.errorLabel)
+        layout.addWidget(self.startMarkerTime)
+        layout.addWidget(self.endMarkerTime)
+        layout.addWidget(self.symmetrizeCheckbox)
+        layout.addWidget(self.convertToGifButton)
+
 
         # Set widget to contain window contents
         wid.setLayout(layout)
@@ -90,24 +143,24 @@ class VideoWindow(QMainWindow):
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
         self.mediaPlayer.error.connect(self.handleError)
 
+        self.enableAllControls(False) # Disable all controls on startup.
     def openFile(self):
         fileName, _ = QFileDialog.getOpenFileName(self, "Import Video",
                 QDir.homePath() + "/Videos",
                 )
 
-
         if fileName != '':
             self.mediaPlayer.setMedia(
                     QMediaContent(QUrl.fromLocalFile(fileName)))
-            self.playButton.setEnabled(True)
+            self.enableAllControls()
             self.mediaPlayer.play()
 
     def openTestFile(self):
+        # NOTE: dev only
         self.mediaPlayer.setMedia(
                 QMediaContent(QUrl.fromLocalFile("C:\\Users\\STUDIUM\\Videos\\Apex Legends\\CHEATER FOOTAGE\\2.mp4")))
-        self.playButton.setEnabled(True)
+        self.enableAllControls()
         self.mediaPlayer.play()
-
 
     def exitCall(self):
         sys.exit(app.exec_())
@@ -137,8 +190,11 @@ class VideoWindow(QMainWindow):
         self.mediaPlayer.setPosition(position)
 
     def handleError(self):
-        self.playButton.setEnabled(False)
+        self.enableAllControls(False)
         self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
+
+    def saveAsGif(self):
+        print("Saving as gif")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
